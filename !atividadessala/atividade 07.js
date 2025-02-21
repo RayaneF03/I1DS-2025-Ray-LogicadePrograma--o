5eb417622629884c815a1e8021a8145f689d3127
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2025-02-21 08:48:50 -0300

    desenvolvimento da atividade 007

--- !atividadessala/atividade 07.js
+++ !atividadessala/atividade 07.js
@@ -1,7 +1,7 @@
 //-------------------------------------------------------------------------------
 //                            lista de exercicios
 //-------------------------------------------------------------------------------
-//dada a matriz abaixo, faça um programa em JavaScript 
+//dada a matriz abaixo, faça um programa em JavaScript
 //que exiba ao usuário os elementos da sua diagonal principal.
 //principal com o laço FOR.
 
@@ -10,15 +10,14 @@
 // |  7   1    4  |
 
 var matriz = [
-    [3, 5, 8],
-    [1, 9, 2],
-    [7, 1, 4],
-  ];
-  for (let linha = 0; linha < matriz.length; linha++) {
-    for (let coluna = 0; coluna < matriz[0].length; coluna++) {
-      console.log(`matriz[${linha}][${coluna}] = ${matriz[linha][coluna]}`);
-    }
+  [3, 5, 8],
+  [1, 9, 2],
+  [7, 1, 4],
+];
+for (let linha = 0; linha < matriz.length; linha++) {
+  for (let coluna = 0; coluna < matriz[0].length; coluna++) {
+    console.log(`matriz[${linha}][${coluna}] = ${matriz[linha][coluna]}`);
   }
-  
-  console.table(matriz);
-  
+}
+console.log(matriz);
+console.table(matriz);
