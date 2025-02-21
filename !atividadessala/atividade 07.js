d2009fea14beac468a01baa068965a58a8c798a5
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2025-02-21 09:07:20 -0300

    reenviado dnv fazer oq

---
+++ !atividadessala/atividade 07.js
@@ -0,0 +1,24 @@
+//-------------------------------------------------------------------------------
+//                            lista de exercicios
+//-------------------------------------------------------------------------------
+//dada a matriz abaixo, faça um programa em JavaScript
+//que exiba ao usuário os elementos da sua diagonal principal.
+//principal com o laço FOR.
+
+// |  3   5    8  |
+// |  1   9    2  |
+// |  7   1    4  |
+
+var matriz = [
+  [3, 5, 8],
+  [1, 9, 2],
+  [7, 1, 4],
+];
+for (let linha = 0; linha < matriz.length; linha++) {
+  for (let coluna = 0; coluna < matriz[0].length; coluna++) {
+    console.log(`matriz[${linha}][${coluna}] = ${matriz[linha][coluna]}`);
+  }
+}
+
+console.log("Elementos da diagonal principal:");
+console.table(matriz);
