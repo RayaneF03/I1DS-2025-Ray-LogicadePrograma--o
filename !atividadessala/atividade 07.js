778a727931e053f4783e554e2f504d8fbcdb4d90
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2025-02-21 10:27:09 -0300

    modificado

--- !atividadessala/atividade 07.js
+++ !atividadessala/atividade 07.js
@@ -8,7 +8,9 @@
 // |  3   5    8  |
 // |  1   9    2  |
 // |  7   1    4  |
-
+console.log(
+  "--------------------------------------------------------------------"
+);
 var matriz = [
   [3, 5, 8],
   [1, 9, 2],
@@ -16,9 +18,45 @@ var matriz = [
 ];
 for (let linha = 0; linha < matriz.length; linha++) {
   for (let coluna = 0; coluna < matriz[0].length; coluna++) {
-    console.log(`matriz[${linha}][${coluna}] = ${matriz[linha][coluna]}`);
+    linha == coluna &&
+      console.log(`matriz[${linha}][${coluna}] = ${matriz[linha][coluna]}`);
   }
 }
-
-console.log("Elementos da diagonal principal:");
 console.table(matriz);
+console.log(
+  "--------------------------------------------------------------------"
+);
+//dada a matriz abaixo, faça um programa em JavaScript
+//que multiplique seus elementos por um valor x e exiba ao usuário
+
+// |  3   5    8  |
+// |  1   9    2  |
+// |  7   1    4  |
+
+var matriz = [
+  [3, 5, 8],
+  [1, 9, 2],
+  [7, 1, 4],
+];
+var x = 4;
+for (let linha = 0; linha < matriz.length; linha++) {
+  for (let coluna = 0; coluna < matriz[0].length; coluna++) {
+    matriz[linha][coluna] *= x;
+  }
+}
+console.table(matriz);
+
+// faça um loop FOR onde apresente os números impares de 0 a 100;
+// não pode ser resolvido, com conta ou resto de divisão
+
+for (let i = 1; i <= 100; i += 2) {
+    console.log(i);}
+    console.log(
+        "--------------------------------------------------------------------"
+      );
+for (let i = 0; i <= 100; i += 2) {
+  console.log(i);
+}
+console.log(
+    "--------------------------------------------------------------------"
+  );
